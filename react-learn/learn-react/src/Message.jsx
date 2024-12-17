b76632e838b34e334e9409f555632d795ba7d764
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2024-12-17 19:48:38 +0800

    Latest

---
+++ react-learn/learn-react/src/Message.jsx
@@ -0,0 +1,17 @@
+
+// Pascal case or PascalCase is a programming
+// naming convention where the first letter
+// of each compound word in a variable is capitalized
+
+function Message(){
+
+
+    // JSX : JavaScript XML
+
+    const name = 'Mark';
+    if(name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World!</h1>
+}
+
+export default Message;
