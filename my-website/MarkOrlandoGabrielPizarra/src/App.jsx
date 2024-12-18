b4f576d893dee3d6d850bfee00edf5a3b7d803e2
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2024-12-19 03:56:40 +0800

    portfolio website

---
+++ my-website/MarkOrlandoGabrielPizarra/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <section className="bg-bannerImg bg-no-repeat bg-cover bg-bottom w-full h-sceen"></section>
+      <div class="bg-[url('background.png')]"></div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
