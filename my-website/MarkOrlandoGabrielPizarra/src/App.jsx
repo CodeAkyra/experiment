f9b23ec1b4afe628e0d891bdaf8a7e224b73f101
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2024-12-20 15:23:07 +0800

    Update App.jsx

--- my-website/MarkOrlandoGabrielPizarra/src/App.jsx
+++ my-website/MarkOrlandoGabrielPizarra/src/App.jsx
@@ -1,9 +1,13 @@
 export default function App() {
+
+const name = 'Mark';
   return (
     <>
       <section className="bg-bannerImg bg-no-repeat bg-cover bg-bottom w-full h-sceen"></section>
       <div class="bg-[url('background.png')]"></div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+<span>Hello Span</span>
+<span>Hello {name}</span>
     </>
   );
 }
